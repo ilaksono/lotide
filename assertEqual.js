bbf2babf6b3ed0f636a63f11c181befc821aa80c
ilaksono <ian.laksono@gmail.com>
2020-09-14 19:48:27 +0000

    created assert function

---
+++ assertEqual.js
@@ -0,0 +1,6 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`) : console.log(`${String.fromCodePoint(0x1F631)} Assertion Failed ${String.fromCodePoint(0x1F631)}: '${actual}' !== '${expected}'`);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
