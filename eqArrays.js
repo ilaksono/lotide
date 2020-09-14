64d173c25bc81e9e5514ee401adcf5d96f00c618
ilaksono <ian.laksono@gmail.com>
2020-09-14 23:52:56 +0000

    moved misplaced files

---
+++ eqArrays.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`) : console.log(`${String.fromCodePoint(0x1F631)} Assertion Failed ${String.fromCodePoint(0x1F631)}: '${actual}' !== '${expected}'`);
+};
+
+const eqArrays = (arr1,arr2) => {
+  let max = arr1.length;
+  if (arr2.length > max)
+    max = arr2.length;
+  for (let i = 0; i < max; i++) {
+    if (arr1[i] !== arr2[i])
+      return false;
+  }
+  return true;
+};
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"])
