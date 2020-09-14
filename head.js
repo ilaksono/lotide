e7c553d546f5cfbd61e9fca1492afa966ca525ed
ilaksono <ian.laksono@gmail.com>
2020-09-14 19:55:00 +0000

    added array head function

---
+++ head.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`) : console.log(`${String.fromCodePoint(0x1F631)} Assertion Failed ${String.fromCodePoint(0x1F631)}: '${actual}' !== '${expected}'`);
+};
+
+const head = (arr) => {
+  return arr[0];
+}
+
+assertEqual(head([]), 5);
