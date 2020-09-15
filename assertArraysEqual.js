a6cb64700b4d40bce7d17600dd338b574a76976e
ilaksono <ian.laksono@gmail.com>
2020-09-15 00:28:36 +0000

    modified array assert function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,7 +13,10 @@ const eqArrays = (arr1,arr2) => {
 };
 
 const assertArraysEqual = (ar1, ar2) => {
-  console.log(eqArrays(ar1, ar2));
+  if(eqArrays(ar1, ar2))
+    console.log(`✅ Assertion Passed ✅: ${ar1} === ${ar2}.`);
+  else  
+    console.log(`${String.fromCodePoint(0x1F631)} Assertion Failed ${String.fromCodePoint(0x1F631)}: ${ar1} !== ${ar2}.`);
   return;
 }
 
