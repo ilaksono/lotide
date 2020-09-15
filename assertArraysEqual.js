1df7a6b7ad3208b82ed4db96242af6535299674b
ilaksono <ian.laksono@gmail.com>
2020-09-15 21:19:51 +0000

    modified flatten and assert arrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,7 +16,7 @@ const assertArraysEqual = (ar1, ar2) => {
   if (eqArrays(ar1, ar2))
     console.log(`✅ Assertion Passed ✅: ${ar1} === ${ar2}.`);
   else
-    console.log(`${String.fromCodePoint(0x1F631)} Assertion Failed ${String.fromCodePoint(0x1F631)}: ${ar1} !== ${ar2}.`);
+    console.log(`${[String.fromCodePoint(0x1F631)]} Assertion Failed ${String.fromCodePoint(0x1F631)}: ${ar1} !== ${ar2}.`);
   return;
 };
 
--- flatten.js
+++ flatten.js
@@ -2,11 +2,20 @@ const flatten = (arr) => {
   let a = [...arr];
   for (let i = 0; i < a.length; i++) {
     while(Array.isArray(a[i])) {
-      temp = a[i];
+      let temp = a[i];
       a.splice(i, 1, ...temp);
     }
   }
   return a;
 };
 
-console.log(flatten([1, 2, [[3], 4], 5, [6]]));
+// const flattenv2 = arr => {
+//   const a = arr.map((a, i, ar) => {
+//     while(Array.isArray(a)) {
+//       let temp = a;
+//       ar.splice(i, 1, ...temp);
+//     }
+//   });
+//   return a;
+// };
+console.log(flatten([1, 2, [[[[3]]], 4], 5, [6]]));
