b0266e05a3a384a296cdc3ab41fe85e0db96de18
ilaksono <ian.laksono@gmail.com>
2020-09-15 00:32:04 +0000

    lint debug

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,11 +13,11 @@ const eqArrays = (arr1,arr2) => {
 };
 
 const assertArraysEqual = (ar1, ar2) => {
-  if(eqArrays(ar1, ar2))
+  if (eqArrays(ar1, ar2))
     console.log(`✅ Assertion Passed ✅: ${ar1} === ${ar2}.`);
-  else  
+  else
     console.log(`${String.fromCodePoint(0x1F631)} Assertion Failed ${String.fromCodePoint(0x1F631)}: ${ar1} !== ${ar2}.`);
   return;
-}
+};
 
 assertArraysEqual([1],[1,2]);
