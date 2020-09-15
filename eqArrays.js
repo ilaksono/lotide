0e8e94f9522107e85e6a57912b4fe17fab3afe0a
ilaksono <ian.laksono@gmail.com>
2020-09-15 00:24:53 +0000

    arrays equal modification

--- eqArrays.js
+++ eqArrays.js
@@ -2,7 +2,7 @@ const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`) : console.log(`${String.fromCodePoint(0x1F631)} Assertion Failed ${String.fromCodePoint(0x1F631)}: '${actual}' !== '${expected}'`);
 };
 
-const eqArrays = (arr1,arr2) => {
+const eqArrays = (arr1, arr2) => {
   let max = -1;
   arr2.length > arr1.length ? max = arr2.length : max = arr1.length;
   if (arr2.length > max)
@@ -15,3 +15,5 @@ const eqArrays = (arr1,arr2) => {
     return false;
   return true;
 };
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
