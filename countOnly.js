3ddd2a7d06af9939275c0c395ea3d62f1e56ab2b
ilaksono <ian.laksono@gmail.com>
2020-09-16 02:26:18 +0000

    Count only object key value pair in array function

---
+++ countOnly.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`) : console.log(`${String.fromCodePoint(0x1F631)} Assertion Failed ${String.fromCodePoint(0x1F631)}: '${actual}' !== '${expected}'`);
+};
+
+const countOnly = (allItems, itemsToCount) => {
+  let count = {};
+  for (const [key,value] of Object.entries(itemsToCount)) {
+    console.log(value);
+    if (value)
+      count[key] = (allItems.reduce((a, val) => val === key ? a + 1 : a, 0));
+    if (!count[key])
+      delete count[key];
+  }
+  console.log(count);
+  return count;
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
