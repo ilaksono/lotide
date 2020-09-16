741012987bd8b0a2143f50f5dfe1085a5b473f36
ilaksono <ian.laksono@gmail.com>
2020-09-16 20:31:52 +0000

    refactored arrays equal boolean function

--- eqArrays.js
+++ eqArrays.js
@@ -2,18 +2,31 @@ const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`) : console.log(`${String.fromCodePoint(0x1F631)} Assertion Failed ${String.fromCodePoint(0x1F631)}: '${actual}' !== '${expected}'`);
 };
 
+// const eqArrays = (arr1, arr2) => {
+//   let max = -1;
+//   arr2.length > arr1.length ? max = arr2.length : max = arr1.length;
+//   if (arr2.length > max)
+//     max = arr2.length;
+//   for (let i = 0; i < max; i++) {
+//     if (arr1[i] !== arr2[i])
+//       return false;
+//   }
+//   if (max === -1)
+//     return false;
+//   return true;
+// };
+
 const eqArrays = (arr1, arr2) => {
-  let max = -1;
-  arr2.length > arr1.length ? max = arr2.length : max = arr1.length;
-  if (arr2.length > max)
-    max = arr2.length;
-  for (let i = 0; i < max; i++) {
-    if (arr1[i] !== arr2[i])
-      return false;
-  }
-  if (max === -1)
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i])
+        return false;
+    }
+  } else
     return false;
-  return true;
+  return true;    
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+
