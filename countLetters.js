ef51eb1d68a0907c6594195185e71597a6c36b3b
ilaksono <ian.laksono@gmail.com>
2020-09-16 17:46:47 +0000

    added possible solution using forEach instead of for

--- countLetters.js
+++ countLetters.js
@@ -3,6 +3,9 @@ const countLetters = (str) => {
   for (let a of str) {
     alphaObj[a] ? alphaObj[a]++ : alphaObj[a] = 1;
   }
+  //OR could use
+  // let arr = [...str];
+  // arr.forEach((val, index) => Number.IsNaN(Number(alphaObj[val])) ? alphaObj[val] = 1 : alphaObj++);
   return alphaObj;
 };
 
