1dbd770087dffb8841ed55b6ad6936b4d59cd242
ilaksono <ian.laksono@gmail.com>
2020-09-17 17:57:52 +0000

    take until higher order function

---
+++ takeUntil.js
@@ -0,0 +1,17 @@
+const takeUntil = (arr, cb) => {
+  let i = 0;
+  while(!cb(arr[i])) {
+    i++;
+  }
+  return arr.slice(0, i);
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
