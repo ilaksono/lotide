5a6df71ce283a412af10f3d57d8d763c7a1938d6
ilaksono <ian.laksono@gmail.com>
2020-09-17 18:12:20 +0000

    find key higher order function

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const findKey = (obj, cb) => {
+  for (let [key, value] of Object.entries(obj)) {
+    if (cb(value))
+      return key;
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`) : console.log(`${String.fromCodePoint(0x1F631)} Assertion Failed ${String.fromCodePoint(0x1F631)}: '${actual}' !== '${expected}'`);
+};
+
+const res = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+// console.log(res);
+
+const test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars > 1);
+
+assertEqual(res, 'noma');
+assertEqual(test, 'Akaleri');
