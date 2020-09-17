10a30e64dac2bdb182b122c6e4159b529e3729b5
ilaksono <ian.laksono@gmail.com>
2020-09-17 03:32:00 +0000

    refactored eqArrays function for readibility

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,23 @@
-const eqArrays = (arr1,arr2) => {
-  let max = -1;
-  arr2.length > arr1.length ? max = arr2.length : max = arr1.length;
-  if (arr2.length > max)
-    max = arr2.length;
-  for (let i = 0; i < max; i++) {
-    if (arr1[i] !== arr2[i])
-      return false;
-  }
-  if (max === -1)
+// const eqArrays = (arr1,arr2) => {
+//   let max = -1;
+//   arr2.length > arr1.length ? max = arr2.length : max = arr1.length;
+//   if (arr2.length > max)
+//     max = arr2.length;
+//   for (let i = 0; i < max; i++) {
+//     if (arr1[i] !== arr2[i])
+//       return false;
+//   }
+//   if (max === -1)
+//     return false;
+//   return true;
+// };
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i])
+        return false;
+    }
+  } else
     return false;
   return true;
 };
