16376d12c21098bbb1eeca3d6f365c51128dfdd0
ilaksono <ian.laksono@gmail.com>
2020-09-19 21:39:21 +0000

    changed eqArrays to only recursion

--- eqArrays.js
+++ eqArrays.js
@@ -16,22 +16,42 @@ const assertEqual = function(actual, expected) {
 //   return true;
 // };
 
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
-        if (!eqArrays(arr1[i], arr2[i]))
-          return false;
-      } else if (arr1[i] !== arr2[i])
-        return false;
-    }
-  } else
-    return false;
+// const eqArrays = (arr1, arr2) => {
+//   if (arr1.length === arr2.length) {
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+//         if (!eqArrays(arr1[i], arr2[i]))
+//           return false;
+//       } else if (arr1[i] !== arr2[i])
+//         return false;
+//     }
+//   } else
+//     return false;
 
-  return true;
+//   return true;
+// };
+
+const eqArrays = (arr1, arr2) => {
+  if(arr1.length === 0 && arr2.length === 0)
+    return true;
+  if (arr1.length !== arr2.length)
+    return false;
+  cur = arr1.shift();
+  can = arr2.shift();
+  if (Array.isArray(cur) !== Array.isArray(can))
+    return false;
+  if (Array.isArray(cur) && Array.isArray(can) && !eqArrays(cur, can))
+    return false;
+  if (cur !== can)
+    return false;
+  return eqArrays(arr1, arr2);
 };
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+assertEqual(eqArrays([1, [2], 3], [1, 2]), false);
 assertEqual(eqArrays([1,[[2],3]],[1,[[2],3]]), true);
+assertEqual(eqArrays([1,[2],3],[1,[2],[3]]), false);
+assertEqual(eqArrays([1,[2],[3]],[1,[2],[3]]), true);
+assertEqual(eqArrays([[[[[[[1]]]]]]], [[[[[1]]]]]), false)
+assertEqual(eqArrays([[[1]]], [[[2]]]), false)
